496256731d3d2398f8d3c43915b5c53f54bb280e
rimsoo <abdeldebeche@gmail.com>
2022-04-03 14:09:05 +0200

    Build Config

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <nav class="w-full p-4 bg-blue-700 sticky top-0">
+      <!-- ici remplacer ce code par celui de ton mon menu -->
+      <div class="text-center text-white text-sm font-semibold">
+        Cette page est en cours de construction...
+      </div>
+    </nav>
+  </header>
+
+  <main class="w-full max-w-screen-lg mx-auto px-6">
+    <!-- ici le contenu de ta page divisé en section -->
+    <HelloWorld />
+  </main>
+
+  <footer>
+    <!-- ici ton footer -->
+  </footer>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="py-10 space-y-8">
+    <h1 class="text-4xl font-semibold">
+      Bonjour le monde 👋
+    </h1>
+    <div class="space-y-6">
+      <p class="text-justify leading-relaxed tracking-wide">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="text-justify leading-relaxed tracking-wide">
+        Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
+</template>
