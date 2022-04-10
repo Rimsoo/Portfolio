8c64e9905799bf5762dd23fdd5e2cb8e6139f9b5
rimsoo <abdeldebeche@gmail.com>
2022-04-10 18:38:32 +0200

    Added name

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,4 +1,9 @@
 <template>
-    <h1>Home</h1>
-    
+    <div class="h-screen">
+        <h1>Home</h1>
+        <div class="grid grid-cols-2 gap-4 w-6/12 h-screen place-content-center my-auto">
+            <h2 class="text-right">Abdel Rahim</h2><h2>DEBECHE</h2>
+            <h2></h2><h2>Expert en Ingenieurie Logiciel</h2>
+        </div>
+    </div>
 </template>
