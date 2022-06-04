c4255233fb3fc0be016ac31ca2d4920c7dae3c61
rimsoo <abdeldebeche@gmail.com>
2022-06-04 22:28:06 +0200

    New test rooter

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
+    base: '/Portfolio/',
     history: createWebHistory(),
     routes,
 });
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: './docs',
-    base: '',
+    base: '/Portfolio/',
   },
   resolve: {
     alias: {
