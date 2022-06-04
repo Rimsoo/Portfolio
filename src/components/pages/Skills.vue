aac3a06febff396466ddb11b6779defed35f9a25
rimsoo <abdeldebeche@gmail.com>
2022-06-05 01:00:23 +0200

    First skill modal [wip]

--- src/components/pages/Skills.vue
+++ src/components/pages/Skills.vue
@@ -13,6 +13,22 @@
                                 <p>{{ skill.title }}</p>
                             </div>
                         </a>
+                        <div class="w-screen h-screen fixed inset-0 flex items-center justify-center" v-if="state.current === skill.id">
+                            <div class="bg-black absolute inset-0 opacity-50" @click="state.current = ''"></div>
+                            <div class="relative max-w-screen-md z-10 bg-white rounded" >
+                                <div class="flex items-center justify-between px-8 py-2 shadow">
+                                    <h3 class="font-bold text-xl">{{ skill.title }}</h3>
+                                    <button class="p-2 hover:bg-red-200 transition duration-100 rounded" @click="state.current = ''">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="px-8 pt-4 pb-8 max-h-[70vh] overflow-y-auto">
+                                    <component :is="skill.modal"></component>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>   
@@ -21,13 +37,17 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { shallowRef, reactive } from 'vue'
 import oop from "@/assets/oop.svg";
 import bdd from "@/assets/bdd.svg";
 import git from "@/assets/git.svg";
 import autonomy from "@/assets/autonomy.svg";
 import perseverance from "@/assets/perseverance.svg";
 import polyvalent from "@/assets/polyvalent.svg";
+
+import Oop from "@/components/skills/Oop.vue";
+
+const OopRef = shallowRef(Oop)
 
 const skills = [
     {
@@ -37,6 +57,7 @@ const skills = [
                 id: 'oop',
                 title: 'Programmation Orientée Objet',
                 image: oop,
+                modal: OopRef
             },
             {
                 id: 'bdd',
